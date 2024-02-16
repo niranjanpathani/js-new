d07233c4924d1a4b25ef98a05adba69d1756aacf
Niranjan A Pathani <141001142+niranjanpathani@users.noreply.github.com>
2024-02-16 19:37:13 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+/*
+alert(3+3) we are using nodejs not browse*/
+
+
+console.log(3+3);
+console.log('Nirnjan');
+
+let name='Niranjan';
+let age=23
+let isLoggedIn = false
+
+// number =>2 to power 53
+//bigint
+// string => ""
+//boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// Object 
+console.log(typeof '3');
+
+console.log(typeof undefined);
+
+console.log(typeof null);
