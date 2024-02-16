9a801e7d7a06998bbdd75a43e4ca3af643dcbcb8
Niranjan A Pathani <141001142+niranjanpathani@users.noreply.github.com>
2024-02-16 19:06:44 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = 'niranjan@gmail.com'
+var accountPassword = '1234'
+acountCity = 'bangalore'
+// accountId = 2
+
+accountEmail='niran@gmail.com'
+accountPassword='21122'
+acountCity='pune'
+let accountState;
+
+console.log(accountId);
+
+
+/*
+prefer not to use var 
+because the var not work in the scope or funtion scope */
+
+console.table([accountId,accountEmail,accountPassword,acountCity ,accountState]);
