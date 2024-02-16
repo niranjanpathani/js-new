3b739c828aca7779f6c04782d6304cb8b4c91cfb
Niranjan A Pathani <141001142+niranjanpathani@users.noreply.github.com>
2024-02-16 19:52:37 +0000

    convertion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "32ab"
+
+
+console.log(typeof score);
+
+let valueIn = Number(score);
+
+console.log(valueIn);
+
+console.log(typeof valueIn);
+
+
+// "33 " => 33
+// "33babs"=> NaN
+// true => 1 ; false => 0
+
+
+let isLoggedIn= 1
+let booleanIsLoggeIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggeIn);
+
+// 1=> true ; 0=> false
+// ""=> false
+// "hi"=> true
+
+
+let someNumber= 3
+
+let stringNumber= String(someNumber);
+
+console.log(stringNumber);
+
+console.log(typeof (stringNumber));
