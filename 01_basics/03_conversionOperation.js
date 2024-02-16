4c8bcc7c23c2a8a3df274d4d92ea6df367ac537c
Niranjan A Pathani <141001142+niranjanpathani@users.noreply.github.com>
2024-02-16 20:35:34 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score = "32ab"
 
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueIn = Number(score);
 
-console.log(valueIn);
+// console.log(valueIn);
 
-console.log(typeof valueIn);
+// console.log(typeof valueIn);
 
 
 // "33 " => 33
@@ -18,7 +18,7 @@ console.log(typeof valueIn);
 let isLoggedIn= 1
 let booleanIsLoggeIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggeIn);
+// console.log(booleanIsLoggeIn);
 
 // 1=> true ; 0=> false
 // ""=> false
@@ -29,6 +29,17 @@ let someNumber= 3
 
 let stringNumber= String(someNumber);
 
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof (stringNumber));
+// console.log(typeof (stringNumber));
+
+
+//********************Operations ************************
+
+let value =4
+let negValue =-value
+
+// console.log(negValue);
+
+ 
+
