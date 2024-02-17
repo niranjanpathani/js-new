e81bbd6af7b6a6465303651ffd9a2c08a540978a
Niranjan A Pathani <141001142+niranjanpathani@users.noreply.github.com>
2024-02-17 12:16:56 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,29 @@
+const name="niranjan"
+
+const repoCount=50
+
+// console.log(name + repoCount + " value");
+// console.log(`Hello my name is ${name}  and my repo cont is ${repoCount}`);
+
+const gameName= new String('Niranjan-p')
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length );
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(4));
+
+console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0,5);
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4);
+console.log(anotherString);
+
+
+const newString1= "    Niranjan   ";
+console.log(newString1);
+console.log(newString1.trim());
