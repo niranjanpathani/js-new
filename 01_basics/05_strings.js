0b8dfbfce0e16c33db05f164d01e7da5a4c71ed7
Niranjan A Pathani <141001142+niranjanpathani@users.noreply.github.com>
2024-02-17 21:06:28 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -27,3 +27,24 @@ console.log(anotherString);
 const newString1= "    Niranjan   ";
 console.log(newString1);
 console.log(newString1.trim());
+
+
+const url ="https://hitesh.com/hitesh%20choudary"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('hitesh'));
+
+console.log(gameName.split('-'));
+
+const sent= `The quick brown fox jumps over the lazy dog and cow.`;
+
+console.log(sent.length);
+
+console.log(sent.charAt(5));
+
+console.log(sent.charCodeAt(4));
+
+console.log(String.fromCharCode(190, 53, 180, 25));
+
+console.log(sent.indexOf('fox'));
