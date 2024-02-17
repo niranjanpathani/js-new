fdb9596d4e7a9826311db4b40d28650f358b42f2
Niranjan A Pathani <141001142+niranjanpathani@users.noreply.github.com>
2024-02-17 09:45:19 +0000

    datatype

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,50 @@
+// Primitive Datatype
+
+// 7 types: String , NUmber , Boolean ,null ,undefined ,symbol, BigInt
+
+const score = 100;
+const scoreV= 10.3
+
+const loggedIn= true
+const temp= null
+
+let userEmail = undefined;
+
+const id = Symbol('123')
+
+const anotherId= Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 738947682923897278739203n
+
+
+
+
+// js is Dynamically typed because in above declarition we no need to specify the type the variables 
+
+
+
+// Non Primitive or reference Datatypes
+
+//Arrays ,Objects, Funtions 
+
+const heros = ["Shaktiman","Nagraj", "doga"];
+
+let myObj={
+    name: "Niranjan",
+    age:22
+
+}
+
+ const myfunction = function (){
+    console.log("hello");
+
+}
+
+myfunction();
+
+console.log(typeof bigNumber);
+
+
+console.log(typeof myfunction);
+
