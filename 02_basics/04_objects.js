b217615bb42436da56a56dd91f9230934d25418b
Niranjan A Pathani <141001142+niranjanpathani@users.noreply.github.com>
2024-03-15 14:40:11 +0000

    04

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,40 @@
+// singleton
+
+const newO= new Object()
+newO.age=23
+
+console.log(newO);
+
+
+const tinder= new Object()
+
+tinder.id="123aba"
+tinder.name="sam"
+tinder.islogged=false
+
+// console.log(tinder);
+const regularUser={
+    email:"google@gamil.ocm",
+    fullname:{
+        userfulname:{
+            firstname:"niranjan",
+            lastname:"pathani"
+        }
+    }
+}
+
+console.log(regularUser.fullname?.userfulname.firstname);
+
+const obj1={
+    1:"a",
+    2:"b"
+}
+const obj2={
+    3:"a",
+    4:"b"
+}
+// const obj3={
+//     obj1,obj2
+// }
+const obj3=Object.assign(obj1,obj2)
+console.log(obj3);
