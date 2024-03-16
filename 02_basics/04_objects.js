d54bac73f9cf6b894f22f1050ee87baebe685fbe
Niranjan A Pathani <141001142+niranjanpathani@users.noreply.github.com>
2024-03-16 16:37:50 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -36,5 +36,8 @@ const obj2={
 // const obj3={
 //     obj1,obj2
 // }
-const obj3=Object.assign(obj1,obj2)
+// const obj3=Object.assign(obj1,obj2)
+// console.log(obj3);
+
+const obj3={...obj1, ...obj2}
 console.log(obj3);
