a2a4b07176adfb4be0875fb0755550366d1cca2c
Niranjan A Pathani <141001142+niranjanpathani@users.noreply.github.com>
2024-03-18 09:32:26 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -33,11 +33,35 @@ const obj2={
     3:"a",
     4:"b"
 }
+
+const obj3={
+    5:"a",
+    6:"b"
+}
 // const obj3={
 //     obj1,obj2
 // }
 // const obj3=Object.assign(obj1,obj2)
 // console.log(obj3);
 
-const obj3={...obj1, ...obj2}
-console.log(obj3);
+// const obj4=Object.assign({},obj1,obj2,obj3)
+const obj4={...obj1, ...obj2,...obj3}
+// const obj3={...obj1, ...obj2}
+// console.log(obj4);
+const users=[
+    {
+        id:1,
+        email:"ab@gamil.com",
+    }
+]
+
+
+
+console.log(tinder);
+console.log(Object.keys(tinder));
+console.log(typeof(Object.keys(tinder)));
+console.log(Object.values(tinder));
+
+console.log(Object.entries(tinder));
+
+console.log(tinder.hasOwnProperty('islogged'));
